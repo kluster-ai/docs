49917284aa214dcec2a91a57b75c364f81e52a83
DAWN KELLY <dawnkelly09@gmail.com>
2025-08-04 12:41:53 -0400

    fixes script

--- js/clipboardCopy.js
+++ js/clipboardCopy.js
@@ -1,32 +1,31 @@
-// Used to copy the llms-full.txt file to the clipboard in the footer
 document.addEventListener('DOMContentLoaded', () => {
   document
-    .querySelector('.llms-copy-button')
+    .querySelector('.full-llms')
     .addEventListener('click', async (event) => {
       try {
         const response = await fetch(
-          'https://docs.kluster.ai/llms-full.txt'
+          'https://docs.kluster.ai/llms-full.txt',
         );
         const text = await response.text();
         await navigator.clipboard.writeText(text);
 
         const copiedToClipboard = document.querySelector('.md-dialog');
-        copiedToClipboard.classList.add('md-dialog--active');
-        const copiedToClipboardMessage =
-          copiedToClipboard.querySelector('.md-dialog__inner');
-        if (copiedToClipboardMessage) {
-          copiedToClipboardMessage.textContent = 'Copied to clipboard';
+        if (copiedToClipboard) {
+          copiedToClipboard.classList.add('md-dialog--active');
+
+          const copiedToClipboardMessage =
+            copiedToClipboard.querySelector('.md-dialog__inner');
+          if (copiedToClipboardMessage) {
+            copiedToClipboardMessage.textContent = "Copied to clipboard";
+          }
+          // Set a timer to remove the after 2 seconds (2000ms)
+          setTimeout(() => {
+            copiedToClipboard.classList.remove('md-dialog--active');
+          }, 2000);
         }
-        // Set a timer to remove the after 2 seconds (2000ms)
-        setTimeout(() => {
-          copiedToClipboard.classList.remove('md-dialog--active');
-        }, 2000);
       } catch (err) {
         console.error('Failed to copy:', err);
-        const copiedToClipboard = document.querySelector('.md-dialog');
-        if (copiedToClipboard) {
-          copiedToClipboard.classList.remove('md-dialog--active');
-        }
+        copiedToClipboard.classList.remove('md-dialog--active');
       }
     });
 });
